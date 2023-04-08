596bb2aada82c8ee5b8b83bc9fbe23415cc3da82
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2023-04-08 23:36:59 +0800

    Update main.cpp

--- ndarray_task/main.cpp
+++ ndarray_task/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include "ndarray.h"
+#include "NDArray.h"
 #include "example.hpp"
 
 int main() {
-    emptyArray();
-    zerosArray();
-    onesArray();
-    randomIntArray();
-    randomFloatArray();
-    addArrays();
-    subArrays();
-    mulArrays();
-    divArrays();
-    matmulArrays();
-    transposeArray();
-    findMin();
-    findMax();
-    findAverage();
+    empty();
+    zeros();
+    ones();
+    randomInt();
+    randomFloat();
+    addition();
+    substruction();
+    multiply();
+    division();
+    matrixmultiply();
+    transposematrix();
+    min();
+    max();
+    average();
     return 0;
 }
